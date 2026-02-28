546b7f6a6a9bd73ac60470f9caae81b2d050db51
lswking1-code <lswking1@gmail.com>
2026-02-28 16:30:06 +0000

    Part A Update 2.1
    Bug fixed

--- DSH NEWS/Assets/Scripts/SaveLoadManage/NextScene.cs
+++ DSH NEWS/Assets/Scripts/SaveLoadManage/NextScene.cs
@@ -3,16 +3,16 @@ using UnityEngine;
 public class NextScene : MonoBehaviour
 {
     public SceneLoadEventSO loadEventSO;
-    public GameSceneSO[] sceneToGo;
+    public GameSceneSO sceneToGo;
     public Vector3 positionToGo;
 
     public void OnNextScene()
     {
-        if (loadEventSO == null || sceneToGo.Length == 0)
+        if (loadEventSO == null || sceneToGo == null)
         {
             return;
         }
-        loadEventSO.RaiseLoadRequestEvent(sceneToGo[0], positionToGo, true);
+        loadEventSO.RaiseLoadRequestEvent(sceneToGo, positionToGo, true);
     }
 
 }

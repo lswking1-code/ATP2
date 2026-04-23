a0ac58be27333be293376cf798f2a0a83b7e48cf
jerrysupreme17 <101377928+jerrysupreme17@users.noreply.github.com>
2026-04-23 18:17:28 +0100

    Part A Update 3.7
    News topic connection

--- DSH NEWS/Assets/SceneA_art/SpriteTalkController.cs
+++ DSH NEWS/Assets/SceneA_art/SpriteTalkController.cs
@@ -17,5 +17,7 @@ public class SpriteTalkController : MonoBehaviour
     public void TalkA() => anim.SetTrigger("TalkA");
     public void TalkB() => anim.SetTrigger("TalkB");
     public void TalkC() => anim.SetTrigger("TalkC");
+    public void TalkD() => anim.SetTrigger("TalkD");
+    public void TalkE() => anim.SetTrigger("TalkE");
     public void StopTalk() => anim.SetTrigger("StopTalk");
 }
